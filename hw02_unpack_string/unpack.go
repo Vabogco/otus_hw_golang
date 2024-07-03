6ceca6b91eeb7f169056fd5f48af8dee9d6379b3
Valeri <51154232+Vabogco@users.noreply.github.com>
2024-07-04 00:31:39 +0200

    HW2 is completed (#2)
    * HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,48 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var result strings.Builder
+	k := false
+
+	for i, j := 0, 1; i < len(s); i, j = i+1, j+1 {
+		if !k {
+			_, err := strconv.Atoi(string(s[i]))
+			if err == nil {
+				return "", ErrInvalidString
+			}
+		}
+
+		b := string(s[i])
+
+		if b == "\\" && !k {
+			k = true
+			continue
+		}
+
+		if j < len(s) {
+			num, err := strconv.Atoi(string(s[j]))
+
+			if err == nil {
+				repstr := strings.Repeat(b, num)
+				result.WriteString(repstr)
+				i++
+				j++
+			} else {
+				result.WriteString(string(s[i]))
+			}
+		} else {
+			result.WriteString(string(s[i]))
+		}
+
+		k = false
+	}
+
+	return result.String(), nil
 }
